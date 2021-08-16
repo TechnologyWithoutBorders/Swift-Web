e2088f0b4ff807165634bfb7b9f9be4fb649c4fb
nitelow <julian.deyerler@teog.de>
2021-08-16 22:45:10 +0200

    implement state update after queueing repair

--- lib/deviceInfoScreen.dart
+++ lib/deviceInfoScreen.dart
@@ -9,12 +9,25 @@ import 'preference_manager.dart' as Prefs;
 import 'deviceInfo.dart';
 import 'deviceStates.dart' as DeviceState;
 
-class DetailScreen extends StatelessWidget {
-  // Declare a field that holds the Todo.
+class DetailScreen extends StatefulWidget {
   final DeviceInfo deviceInfo;
 
-  // In the constructor, require a Todo.
   DetailScreen({Key key, @required this.deviceInfo}) : super(key: key);
+
+  @override
+  _DetailScreenState createState() => _DetailScreenState(deviceInfo: deviceInfo);
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  DeviceInfo deviceInfo;
+
+  _DetailScreenState({this.deviceInfo});
+
+  _updateDeviceInfo(DeviceInfo deviceInfo) {
+    setState(() {
+      this.deviceInfo = deviceInfo;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +35,7 @@ class DetailScreen extends StatelessWidget {
 
     String reasonText;
 
-    switch(deviceInfo.report.currentState) {
+    switch(widget.deviceInfo.report.currentState) {
       case DeviceState.broken:
         reasonText = "The defect has already been reported.";
         break;
@@ -37,13 +50,13 @@ class DetailScreen extends StatelessWidget {
     if(reasonText != null) {
       reportWidget = Text(reasonText, style: TextStyle(fontSize: 20));
     } else {
-      reportWidget = ReportProblemForm();
+      reportWidget = ReportProblemForm(deviceInfo: widget.deviceInfo, updateDeviceInfo: _updateDeviceInfo,);
     }
 
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text(deviceInfo.device.type),
+        title: Text(widget.deviceInfo.device.type),
       ),
       body: Center(child: FractionallySizedBox(widthFactor: 0.65, heightFactor: 0.8,
         child: Card(
@@ -51,20 +64,20 @@ class DetailScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(deviceInfo.device.manufacturer + " " + deviceInfo.device.model, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                Text(deviceInfo.device.location, style: TextStyle(fontSize: 25)),
+                Text(widget.deviceInfo.device.manufacturer + " " + widget.deviceInfo.device.model, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                Text(widget.deviceInfo.device.location, style: TextStyle(fontSize: 25)),
                 SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(height: 350,
-                      child: Image.memory(base64Decode(deviceInfo.imageData)),
+                      child: Image.memory(base64Decode(widget.deviceInfo.imageData)),
                     ),
                     SizedBox(width: 30),
                     Column(mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                       SizedBox(height: 5),
-                      StateScreen(deviceInfo: deviceInfo),
+                      StateScreen(deviceInfo: widget.deviceInfo),
                       SizedBox(height: 20),
                       reportWidget,
                     ])
@@ -72,7 +85,7 @@ class DetailScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
                 Text("Available Documents:", style: TextStyle(fontSize: 20)),
-                DocumentScreen(deviceInfo: deviceInfo),
+                DocumentScreen(deviceInfo: widget.deviceInfo),
               ]
             )
           ),
@@ -88,24 +101,20 @@ class DocumentScreen extends StatefulWidget {
   DocumentScreen({Key key, @required this.deviceInfo}) : super(key: key);
 
   @override
-  _DocumentScreenState createState() => _DocumentScreenState(deviceInfo: deviceInfo);
+  _DocumentScreenState createState() => _DocumentScreenState();
 }
 
 class _DocumentScreenState extends State<DocumentScreen> {
-  final DeviceInfo deviceInfo;
-
   List<String> _documents = [];
 
-  _DocumentScreenState({this.deviceInfo});
-
   void _retrieveDocuments() {
-    Comm.retrieveDocuments(deviceInfo.device.manufacturer, deviceInfo.device.model).then((documents) {//TODO catch Exception
+    Comm.retrieveDocuments(widget.deviceInfo.device.manufacturer, widget.deviceInfo.device.model).then((documents) {//TODO catch Exception
       setState(() { _documents = documents; });
     });
   }
 
   void _downloadDocument(String docName) {
-    String url = "https://teog.virlep.de/interface/3/device_documents/" + deviceInfo.device.manufacturer + "/" + deviceInfo.device.model + "/" + docName;
+    String url = "https://teog.virlep.de/interface/3/device_documents/" + widget.deviceInfo.device.manufacturer + "/" + widget.deviceInfo.device.model + "/" + docName;
     html.AnchorElement anchorElement =  new html.AnchorElement(href: url);
     anchorElement.download = url;
     anchorElement.click();
@@ -152,13 +161,10 @@ class StateScreen extends StatefulWidget {
   StateScreen({Key key, @required this.deviceInfo}) : super(key: key);
 
   @override
-  _StateScreenState createState() => _StateScreenState(deviceInfo: deviceInfo);
+  _StateScreenState createState() => _StateScreenState();
 }
 
 class _StateScreenState extends State<StateScreen> {
-  final DeviceInfo deviceInfo;
-
-  _StateScreenState({this.deviceInfo});
 
   @override
   Widget build(BuildContext context) {
@@ -169,12 +175,12 @@ class _StateScreenState extends State<StateScreen> {
           .textTheme
           .headline5),
         SizedBox(height: 5),
-          Container(color: deviceInfo.report.getColor(),
+          Container(color: widget.deviceInfo.report.getColor(),
           child: Padding(padding: EdgeInsets.all(3.0),
             child: Row(children: [
-                Icon(deviceInfo.report.getIconData()),
+                Icon(widget.deviceInfo.report.getIconData()),
               SizedBox(width: 5),
-              Text(deviceInfo.report.getStateString(),
+              Text(widget.deviceInfo.report.getStateString(),
                 style: TextStyle(fontSize: 25)
               ),
               ]
@@ -182,13 +188,19 @@ class _StateScreenState extends State<StateScreen> {
           )
         ),
         SizedBox(height: 5),
-        Text(deviceInfo.report.created),
+        Text(widget.deviceInfo.report.created),
       ]
     );
   }
 }
 
 class ReportProblemForm extends StatefulWidget {
+  final DeviceInfo deviceInfo;
+
+  final ValueChanged<DeviceInfo> updateDeviceInfo;
+
+  ReportProblemForm({Key key, @required this.deviceInfo, this.updateDeviceInfo}) : super(key: key);
+
   @override
   _ReportProblemFormState createState() => _ReportProblemFormState();
 }
@@ -202,8 +214,7 @@ class _ReportProblemFormState extends State<ReportProblemForm> {
   void _createReport() {
     if (_formKey.currentState.validate()) {
       Comm.queueRepair(479, _reportTitleController.text, _problemTextController.text).then((newReport) {
-        //TODO: Gerätedaten aktualisieren
-        //setState(() { deviceInfo = modifiedDeviceInfo; });
+        setState(() { widget.updateDeviceInfo(DeviceInfo(device: widget.deviceInfo.device, report: newReport, imageData: widget.deviceInfo.imageData)); });
       }).onError((error, stackTrace) {
         final snackBar = SnackBar(content: Text(error.data));
         ScaffoldMessenger.of(context).showSnackBar(snackBar);
