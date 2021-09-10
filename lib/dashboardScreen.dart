824306c4e4169b74094c150577ef48032213e78c
nitelow <julian.deyerler@teog.de>
2021-09-10 18:47:57 +0200

    formatting

--- lib/dashboardScreen.dart
+++ lib/dashboardScreen.dart
@@ -48,51 +48,56 @@ class _DetailScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: SizedBox(width: 400, height: 600,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: [Card(
-          child: Padding(padding: EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("TODO"),
-                SizedBox(height: 500, width: 600, child:
-                  Scrollbar(isAlwaysShown: true,
-                    controller: _scrollController,
-                    child: ListView.separated(
-                      controller: _scrollController,
-                      padding: const EdgeInsets.all(8),
-                      itemCount: _devices.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          leading: Container(width: 30, height: 30, color: _devices[index].report.getColor(),
-                            child: Padding(padding: EdgeInsets.all(3.0),
-                              child: Row(children: [
-                                  Icon(_devices[index].report.getIconData()),
-                                ]
-                              )
-                            )
+    return Scaffold(
+      body: Center(
+        child: SizedBox(width: 400, height: 600,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: Padding(padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("TODO"),
+                      SizedBox(height: 500, width: 600,
+                        child: Scrollbar(isAlwaysShown: true,
+                          controller: _scrollController,
+                          child: ListView.separated(
+                            controller: _scrollController,
+                            padding: const EdgeInsets.all(8),
+                            itemCount: _devices.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(
+                                leading: Container(width: 30, height: 30, color: _devices[index].report.getColor(),
+                                  child: Padding(padding: EdgeInsets.all(3.0),
+                                    child: Row(children: [
+                                        Icon(_devices[index].report.getIconData()),
+                                      ]
+                                    )
+                                  )
+                                ),
+                                title: Text(_devices[index].device.type),
+                                subtitle: Text(_devices[index].device.manufacturer + " " + _devices[index].device.model),
+                                trailing: Text(_devices[index].device.location),
+                                onTap: () => _openDeviceById(_devices[index].device.id)
+                              );
+                            },
+                            separatorBuilder: (BuildContext context, int index) => const Divider(),
                           ),
-                          title: Text(_devices[index].device.type),
-                          subtitle: Text(_devices[index].device.manufacturer + " " + _devices[index].device.model),
-                          trailing: Text(_devices[index].device.location),
-                          onTap: () => _openDeviceById(_devices[index].device.id)
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) => const Divider(),
-                    ),
-                  ),
+                        ),
+                      ),
+                      TextButton(
+                        child: Text('Create new device?'),
+                      ),
+                    ]
+                  )
                 ),
-                TextButton(
-                  child: Text('Create new device?'),
-                ),
-              ]
-            )
-          ),
-        )]
+              )
+            ]
+          )
+        )
       )
-      )));
+    );
   }
 }
