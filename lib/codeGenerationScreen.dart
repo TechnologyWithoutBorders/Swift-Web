1de2f22151111fddcee1a9971183d0d76ea93e81
nitelow <julian.deyerler@teog.de>
2021-09-23 01:08:30 +0200

    implement pdf generation

--- lib/codeGenerationScreen.dart
+++ lib/codeGenerationScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:teog_swift/main.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,9 @@ import 'package:teog_swift/previewDeviceInfo.dart';
 import 'package:teog_swift/deviceInfoScreen.dart';
 
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pdfLib;
+import 'package:printing/printing.dart';
 
 class CodeGenerationScreen extends StatefulWidget {
   CodeGenerationScreen({Key key}) : super(key: key);
@@ -20,45 +24,90 @@ class CodeGenerationScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<CodeGenerationScreen> {
-  final _scrollController = ScrollController();
+  final _formKey = GlobalKey<FormState>();
 
-  List<DeviceInfo> _devices = [];
+  final _amountTextController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
+  void _generateBarcodes() async {
+    if(_formKey.currentState.validate()) {
+      int amount = int.parse(_amountTextController.text);
 
-    Comm.getTodoDevices().then((devices) {//TODO catch Exception
-      setState(() {
-        _devices = devices;
-      });
-    });
-  }
+      final font = await rootBundle.load("OpenSans-Regular.ttf");
+      final ttf = pdfLib.Font.ttf(font);
+      final fontBold = await rootBundle.load("OpenSans-Bold.ttf");
+      final ttfBold = pdfLib.Font.ttf(fontBold);
+      final fontItalic = await rootBundle.load("OpenSans-Italic.ttf");
+      final ttfItalic = pdfLib.Font.ttf(fontItalic);
+      final fontBoldItalic =
+          await rootBundle.load("OpenSans-BoldItalic.ttf");
+      final ttfBoldItalic = pdfLib.Font.ttf(fontBoldItalic);
+      final pdfLib.ThemeData themeData = pdfLib.ThemeData.withFont(
+        base: ttf,
+        bold: ttfBold,
+        italic: ttfItalic,
+        boldItalic: ttfBoldItalic,
+      );
 
-  void _openDeviceById(int id) {
-    Comm.fetchDevice(id).then((deviceInfo) {//TODO catch Exception
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => DetailScreen(deviceInfo: deviceInfo),
+      final pdf = pdfLib.Document();
+
+      pdf.addPage(
+        pdfLib.Page(
+          theme: themeData,//TODO: vs pageTheme?
+          pageFormat: PdfPageFormat.a4,
+          build: (pdfLib.Context context) {
+            return pdfLib.Center(
+              child: pdfLib.Text("Hello World"),
+            ); // Center
+          }
         )
       );
-    });
+
+      await Printing.sharePdf(bytes: await pdf.save(), filename: 'barcodes.pdf');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: SizedBox(width: 400, height: 600,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: [QrImage(
-          data: "1234567890",
-          version: QrVersions.auto,
-          size: 200.0)
-          ]
+    return Scaffold(
+      body: Center(
+        child: SizedBox(width: 400, height: 600,
+          child: Form(key: _formKey,
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Generate a printable document', style: Theme
+                  .of(context)
+                  .textTheme
+                  .headline5),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _amountTextController,
+                  decoration: InputDecoration(hintText: 'Amount of barcodes'),
+                  validator: (value) {
+                    if (value.isEmpty || int.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                  onFieldSubmitted: (value) => _generateBarcodes(),
+                ),
+                SizedBox(height: 5),
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateColor.resolveWith((Set<MaterialState> states) {
+                      return states.contains(MaterialState.disabled) ? null : Colors.white;
+                    }),
+                    backgroundColor: MaterialStateColor.resolveWith((Set<MaterialState> states) {
+                      return states.contains(MaterialState.disabled) ? null : Color(0xff667d9d);
+                    }),
+                  ),
+                  onPressed: () => _generateBarcodes(),
+                  child: Text('Generate'),
+                )
+              ]
+            )
           )
-          ),
-        )
-      );
+        ),
+      )
+    );
   }
 }
