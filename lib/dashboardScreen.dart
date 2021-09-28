4041108f186fd071843461cc81ab7ebfc421cb7b
nitelow <julian.deyerler@teog.de>
2021-09-28 20:56:12 +0200

    layout improvements

--- lib/dashboardScreen.dart
+++ lib/dashboardScreen.dart
@@ -93,7 +93,7 @@ class _DetailScreenState extends State<DashboardScreen> {
                           .of(context)
                           .textTheme
                           .headline4),
-                        Flexible(child: Padding(padding: EdgeInsets.all(60.0), child: DatumLegendWithMeasures(seriesList))),
+                        Flexible(child: Padding(padding: EdgeInsets.all(30.0), child: DatumLegendWithMeasures(seriesList))),
                         Text("xy devices are due for maintenance", style: Theme
                           .of(context)
                           .textTheme
@@ -105,7 +105,7 @@ class _DetailScreenState extends State<DashboardScreen> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("TODO", style: Theme
+                        Text("ToDo", style: Theme
                           .of(context)
                           .textTheme
                           .headline4),
