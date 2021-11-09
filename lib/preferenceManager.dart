9dc6533df6e178929a4807af3e0343a8356f0fab
nitelow <julian.deyerler@teog.de>
2021-11-09 18:37:04 +0100

    add documentation

--- lib/preferenceManager.dart
+++ lib/preferenceManager.dart
@@ -3,6 +3,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'networkFunctions.dart' as Comm;
 import 'constants.dart';
 
+/// Checks whether the login data is present within the shared preferences.
+///
+/// If [syncWithServer] is set to true, the login data is checked on the server.
 Future<bool> checkLogin({bool syncWithServer: false}) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
@@ -23,6 +26,7 @@ Future<bool> checkLogin({bool syncWithServer: false}) async {
   return false;
 }
 
+/// Saves [country], [hospital] and [password] in shared preferences.
 Future<bool> save(String country, int hospital, String password) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
