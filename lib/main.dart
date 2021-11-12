fd0214bbdf1db8fed9a5832427366fec142b5141
nitelow <julian.deyerler@teog.de>
2021-11-12 01:50:42 +0100

    extend role handling

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class _LoginFormState extends State<LoginForm> {
 
         if(role == Constants.role_technical) {
           route = TabScreen.route;
-        } else {
+        } else if(role == Constants.role_medical) {
           route = OverviewScreen.route;
         }
 
