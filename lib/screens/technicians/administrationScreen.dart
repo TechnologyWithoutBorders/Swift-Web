dae01e164587b4caeadb5ffcf0b24b6e97bbfb7e
nitelow <julian.deyerler@teog.de>
2022-01-18 01:32:32 +0100

    remove obsolete code

--- lib/screens/technicians/administrationScreen.dart
+++ lib/screens/technicians/administrationScreen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:graphview/GraphView.dart';
 
 import 'dart:html' as html;
 
 import 'package:teog_swift/utilities/networkFunctions.dart' as Comm;
-import 'package:teog_swift/utilities/organizationalRelation.dart';
-import 'package:teog_swift/utilities/organizationalUnit.dart';
 import 'package:teog_swift/utilities/user.dart';
 import 'package:teog_swift/utilities/hospital.dart';
 import 'package:teog_swift/utilities/messageException.dart';
@@ -27,8 +24,6 @@ class _DetailScreenState extends State<UserManagementScreen> {
   final _scrollController = ScrollController();
 
   Hospital _hospital;
-  Graph _graph = Graph();
-  Map<int, String> _nameMap = Map();
   List<User> _users = [];
 
   void _createUser() {
@@ -51,31 +46,6 @@ class _DetailScreenState extends State<UserManagementScreen> {
     Comm.getHospitalInfo().then((hospital) {
       setState(() {
         _hospital = hospital;
-      });
-    }).onError<MessageException>((error, stackTrace) {
-        final snackBar = SnackBar(content: Text(error.message));
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    });
-
-    Comm.getOrganizationalInfo().then((orgInfo) {
-      Graph graph = Graph();
-
-      Map<int, String> nameMap = Map();
-
-      for(OrganizationalUnit orgUnit in orgInfo.units) {
-        Node node = Node.Id(orgUnit.id);
-        graph.addNode(node);
-
-        nameMap[orgUnit.id] = orgUnit.name;
-      }
-
-      for(OrganizationalRelation orgRelation in orgInfo.relations) {
-          graph.addEdge(graph.getNodeUsingId(orgRelation.parent), graph.getNodeUsingId(orgRelation.id));
-      }
-
-      setState(() {
-        _graph = graph;
-        _nameMap = nameMap;
       });
     }).onError<MessageException>((error, stackTrace) {
         final snackBar = SnackBar(content: Text(error.message));
@@ -117,120 +87,8 @@ class _DetailScreenState extends State<UserManagementScreen> {
     );
   }
 
-  void _reOrganizeUnit(int id, int parentId) {
-    Node parent = _graph.getNodeUsingId(parentId);
-    Node child = _graph.getNodeUsingId(id);
-
-    List<Node> successors = _graph.successorsOf(child);
-
-    if(!successors.contains(parent)) {
-      setState(() {
-        _graph.removeEdges(_graph.getInEdges(child));
-
-        _graph.addEdge(parent, child);
-      });
-    } else {
-      final snackBar = SnackBar(content: Text("cannot set a node as its own successor"));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-  }
-
-  void _addUnit(int parent) {
-    //TODO: should those be disposed?
-    TextEditingController nameController = TextEditingController();
-
-    showDialog<String>(
-      context: context,
-      builder: (BuildContext context) {
-        return new AlertDialog(
-          title: Text("Add child unit to \"" + _nameMap[parent] + "\""),
-          contentPadding: const EdgeInsets.all(16.0),
-          content: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                controller: nameController,
-                decoration: new InputDecoration(
-                  labelText: 'Name of child unit'),
-                autofocus: true,
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            ElevatedButton(
-                child: const Text('Cancel'),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            ElevatedButton(
-                child: const Text('Create'),
-                onPressed: () {
-                  if(nameController.text.isNotEmpty) {
-                    int maxId = 1;
-
-                    for(Node node in _graph.nodes) {
-                      if(node.key.value > maxId) {
-                        maxId = node.key.value;
-                      }
-                    }
-
-                    int newId = maxId+1;
-
-                    Node node = Node.Id(newId);
-
-                    //TODO: magic
-                    setState(() {
-                      _graph.addNode(node);
-                      _graph.addEdge(_graph.getNodeUsingId(parent), node);
-                      _nameMap[newId] = nameController.text;
-                    });
-
-                    Navigator.pop(context);
-                  }
-                })
-          ],
-        );
-      }
-    );
-  }
-
-  void _removeUnit(int id) {
-    showDialog<String>(
-      context: context,
-      builder: (BuildContext context) {
-        return new AlertDialog(
-          title: Text("Delete unit \"" + _nameMap[id] + "\"?"),
-          content: Text("This will also delete all child units.", style: TextStyle(color: Colors.red)),
-          actions: <Widget>[
-            ElevatedButton(
-                child: const Text('Cancel'),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            ElevatedButton(
-                child: const Text('Delete'),
-                onPressed: () {
-                  Node node = _graph.getNodeUsingId(id);
-                  List<Node> successors = _graph.successorsOf(node);
-
-                  setState(() {
-                    _graph.removeNodes(successors);
-                    _graph.removeNode(node);
-                    _nameMap.remove(id);
-                  });
-
-                  Navigator.pop(context);
-                })
-          ],
-        );
-      }
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    BuchheimWalkerConfiguration builder = BuchheimWalkerConfiguration();
-
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: Container(alignment: Alignment.center,
