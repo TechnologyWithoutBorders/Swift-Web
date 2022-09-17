36f3a2b6ed915fdfa49225e1ab84369508ee34e5
nitelow <julian.deyerler@teog.de>
2022-09-17 23:54:36 +0200

    bump interface version

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const int interfaceVersion = 4;
+  static const int interfaceVersion = 5;
 
   static const String key_pw = "password";
   static const String key_country = "country";
