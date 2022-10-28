85365ff8ee38852b9d206a1cdbffdef0cfb15f88
nitelow <julian.deyerler@teog.de>
2022-10-29 01:24:00 +0200

    plot report history

--- lib/screens/reportHistoryPlot.dart
+++ lib/screens/reportHistoryPlot.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:teog_swift/utilities/deviceInfo.dart';
+import 'package:teog_swift/utilities/detailedReport.dart';
+import 'package:teog_swift/utilities/deviceState.dart';
 
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:teog_swift/utilities/hospitalDevice.dart';
 import 'package:teog_swift/utilities/networkFunctions.dart' as Comm;
 import 'package:teog_swift/utilities/organizationalUnit.dart';
 import 'package:teog_swift/utilities/messageException.dart';
@@ -15,17 +18,77 @@ class ReportHistoryPlot extends StatefulWidget {
 }
 
 class _ReportHistoryPlotState extends State<ReportHistoryPlot> {
-  List<DeviceInfo>? _deviceInfos;
-
-  final _orgScrollController = ScrollController();
+  List<charts.Series<CategoryData, int>>? _seriesList;
 
   @override
   void initState() {
     super.initState();
 
     Comm.getAllDeviceInfos().then((deviceInfos) {
+      DateTime earliest = DateTime.now();
+
+      //find earliest report
+      for(var deviceInfo in deviceInfos) {
+        List<DetailedReport> reports = deviceInfo.reports;
+
+        if(reports.last.created.isBefore(earliest)) {
+          earliest = DateTime(reports.last.created.year, reports.last.created.month, reports.last.created.day);
+        }
+      }
+      
+      //iterate over days
+      DateTime now = DateTime.now();
+      DateTime today = DateTime(now.year, now.month, now.day);
+      int days = today.difference(earliest).inDays;
+
+      List<List<CategoryData>> dataList = [[], [], [], [], [], []];
+
+      for(int day = 0; day < days; day++) {
+        DateTime current = earliest.add(Duration(days: day));
+
+        var stateCounters = [0, 0, 0, 0, 0, 0];
+
+        //find current state of every device
+        for(var deviceInfo in deviceInfos) {
+          List<DetailedReport> reports = deviceInfo.reports;
+          reports = reports.reversed.toList();
+
+          DetailedReport lastReport = reports.first;
+
+          for(var report in reports) {
+            if(report.created.isBefore(current)) {
+              if(report.created.isAfter(lastReport.created)) {
+                lastReport = report;
+              }
+            } else {
+              break;
+            }
+          }
+
+          stateCounters[lastReport.currentState] = stateCounters[lastReport.currentState]+1;
+        }
+
+        for(int state = 0; state < 6; state++) {
+          dataList[state].add(new CategoryData(day, stateCounters[state], charts.ColorUtil.fromDartColor(DeviceState.getColor(state))));
+        }
+      }
+
+      List<charts.Series<CategoryData, int>> seriesList = [];
+
+      for(int state = 0; state < 6; state++) {
+        seriesList.add(
+          new charts.Series<CategoryData, int>(
+            id: DeviceState.getStateString(state),
+            domainFn: (CategoryData categoryData, _) => categoryData.category,
+            measureFn: (CategoryData categoryData, _) => categoryData.count,
+            colorFn: (CategoryData categoryData, _) => categoryData.color,
+            data: dataList[state],
+            labelAccessorFn: (CategoryData categoryData, _) => '${categoryData.category}: ${categoryData.count}',
+        ));
+      }
+
       setState(() {
-        _deviceInfos = deviceInfos;
+        _seriesList = seriesList;
       });
     }).onError<MessageException>((error, stackTrace) {
         final snackBar = SnackBar(content: Text(error.message));
@@ -41,24 +104,34 @@ class _ReportHistoryPlotState extends State<ReportHistoryPlot> {
         child: Card(
           child: Padding(
             padding: EdgeInsets.all(25.0),
-            child: _deviceInfos != null ? Center(
-              child: Scrollbar(
-                controller: _orgScrollController,
-                child: SingleChildScrollView(
-                  controller: _orgScrollController,
-                  scrollDirection: Axis.horizontal,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      
-                    ]
-                  )
-                )
-              )
-            ) : Center(child: Text("crunching data..."))
+            child: _seriesList != null ? Center(
+              child: Center(child: StateLineChart(_seriesList!))
+            ) : Center(child: SizedBox(width: 60, height: 60, child: CircularProgressIndicator()))
           )
         )
       )
     );
   }
+}
+
+class StateLineChart extends StatelessWidget {
+  final List<charts.Series<dynamic, int>> seriesList;
+
+  StateLineChart(this.seriesList);
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.LineChart(
+      seriesList,
+      defaultRenderer: new charts.LineRendererConfig(includeArea: true, stacked: true),
+    );
+  }
+}
+
+class CategoryData {
+  final int category;
+  final int count;
+  final charts.Color color;
+
+  CategoryData(this.category, this.count, this.color);
 }
