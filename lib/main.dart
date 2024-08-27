7eb89f234f49f236401b19cf18429136d65c758b
nitelow <julian.deyerler@teog.de>
2024-08-27 19:31:45 +0545

    increase bottom space

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   child: const Text('About'),
                 ),
+                const SizedBox(height: 150)
               ],
             ),
           ),
